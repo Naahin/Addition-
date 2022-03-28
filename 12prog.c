318370f6207095e18cc2014072e2e103e2bc886b
Naahin <98544401+Naahin@users.noreply.github.com>
2022-03-28 22:22:31 +0600

    Add files via upload

---
+++ 12prog.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b,c;
+    printf("\nEnter value for a: ");
+    scanf("%d",&a);
+    printf("\nEnter value for b: ");
+    scanf("%f",&b);
+    c=a+b;
+    printf("devision result: %f",c);
+
+
+}
